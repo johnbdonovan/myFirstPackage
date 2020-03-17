684acdb9970c5876c30b9aa89b82764335578f72
John Donovan <johnbdonovan13@gmail.com>
2020-03-17 07:52:07 -0700

    RStudio Commit

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -4,8 +4,13 @@ test_that("multiplication works", {
 test_that("my_lm works mathematically", {
   expect_equal(my_lm(y ~ x, data.frame(x = c(1, 2), y = c(2, 4)))$Estimate,
                c(0, 2))
+  expect_equal(my_lm(y ~ x, data.frame(x = c(1, 2), 3, 4,
+                                       y = c(0, 0, 0, 0)))$Std.Error,
+               c(0, 0))
   expect_equal(my_lm(y ~ x, data.frame(x = c(1, 2), y = c(2, 4)))$t_value,
                c(0, 0))
+  expect_equal(my_lm(y ~ x, data.frame(x = c(1, 2, 3, 4, 5),
+                                       y = c(5, 4, 3, 2, 1)))$pVal, c(0, 0))
 })
 test_that("non formula throws error", {
   expect_error(my_lm(Sepal.Length, my_iris))
