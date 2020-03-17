8d61d005817251d5b9d0cb2251060c7fd2fed04b
John Donovan <johnbdonovan13@gmail.com>
2020-03-17 07:39:50 -0700

    RStudio commit

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -2,7 +2,10 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 test_that("my_lm works mathematically", {
-
+  expect_equal(my_lm(y ~ x, data.frame(x = c(1, 2), y = c(2, 4)))$Estimate,
+               c(0, 2))
+  expect_equal(my_lm(y ~ x, data.frame(x = c(1, 2), y = c(2, 4)))$t_value,
+               c(0, 0))
 })
 test_that("non formula throws error", {
   expect_error(my_lm(Sepal.Length, my_iris))
